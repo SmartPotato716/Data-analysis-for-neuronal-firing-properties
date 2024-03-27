7f80d262b70b13b76155a3e99c358819d19342b5
SmartPotato716 <124715659+SmartPotato716@users.noreply.github.com>
2024-03-27 12:49:55 -0400

    Create Filepathes.py

---
+++ Filepathes.py
@@ -0,0 +1,5 @@
+InputOutput_BatchExport_path = '/content/drive/MyDrive/Ephys/BatchOutput/InputOutput_BatchExport'
+PhasePlot_BatchExport_path = '/content/drive/MyDrive/Ephys/BatchOutput/PhasePlot_BatchExport'
+
+folder_path = '/content/drive/MyDrive/Ephys/RawData'
+Output_path = "/content/drive/MyDrive/Ephys"
